25d61f6f730edbfd4e0bac1d45762a8550673662
Santiago Pérez Gómez <45902173+SantiPG96@users.noreply.github.com>
2018-12-16 13:49:42 +0100

    Prueba conflicto

--- PracticaGrupalPeliculas/src/ventanas/Login.java
+++ PracticaGrupalPeliculas/src/ventanas/Login.java
@@ -6,8 +6,7 @@
 package ventanas;
 
 /**
- *
- * Clase principal
+ * Conflicto
  * @author Santiago Pérez Gómez, Sergio Martín Hernández, Sergio Rivera Gálvez
  */
 public class Login extends javax.swing.JFrame {
